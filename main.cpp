4860710c21a99a53fdb1ede226e148724660fdcc
TheLastGimbus <mateusz.soszynski.2003@gmail.com>
2018-10-27 00:10:06 +0200

    First time on git!

---
+++ main.cpp
@@ -0,0 +1,273 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ArduinoJson.h>
+#include "FS.h"
+
+String softwareVersion = "1.0";
+
+
+#define PIN_MAIN 13
+#define PIN_LED_GREEN 14
+#define PIN_LED_RED 12
+#define PIN_STANDBY 5
+#define PIN_FACTORY_RESET 16
+
+ADC_MODE(ADC_VCC);
+
+String wifiName = "";
+String wifiPass = "";
+
+unsigned long timerGoToSleep = 0;
+
+bool tryingToConnect = 0;
+
+bool weNeedToSend = 0;
+String messageToSend = "";
+unsigned long sendingLastTry = 0;
+unsigned long sendingCount = 0;
+
+unsigned long timersButton[16]; // timers
+
+#define TAG_DATA 1
+#define TAG_EVENT 2
+#define TAG_ERROR 3
+void Log(int tag, String message){
+	Serial.println(String(tag) + ": " + message);
+}
+
+void goToSleep(){
+	Log(TAG_EVENT, "Going to sleep...");
+	delay(10);
+	digitalWrite(PIN_STANDBY, 0);
+	delay(15000);
+}
+
+void setLed(bool green, bool red){
+	digitalWrite(PIN_LED_GREEN, green);
+	if(red){
+		analogWrite(PIN_LED_RED, 180);
+	}
+	else{
+		analogWrite(PIN_LED_RED, 0);
+	}
+}
+
+void doingSomething(){
+	timerGoToSleep = millis();
+}
+
+void resetConfigFile(String ssid = "Broadcom", String pass = ""){
+	if(SPIFFS.begin()){
+		SPIFFS.remove("config.json");
+		File config = SPIFFS.open("config.json", "w");
+		config.println("{");
+		config.println("    \"ssid\": \""+ssid+"\",");
+		config.println("    \"password\": \""+pass+"\"");
+		config.println("}");
+		config.flush();
+		config.close();
+		Log(TAG_EVENT, "New config file created");
+	}
+	else{
+		Log(TAG_ERROR, "SPIFFS begin fail");
+	}
+	SPIFFS.end();
+}
+
+void loadDataFromFS(){
+	Log(TAG_EVENT, "Loading DATA from FS...");
+	if(SPIFFS.begin()){
+		if(SPIFFS.exists("config.json")){
+			File file = SPIFFS.open("config.json", "r");
+			if(file && file.size() > 0){
+				StaticJsonBuffer<600> jsonBuffer;
+				JsonObject& json = jsonBuffer.parseObject(file.readString().c_str());
+				if(json.success()){
+					const char* ssid = json["ssid"];
+					const char* password = json["password"];
+					Log(TAG_DATA, "SSID from JSON: " + String(ssid));
+					Log(TAG_DATA, "Password from JSON: " + String(password));
+					wifiName = ssid;
+					wifiPass = password;
+					Log(TAG_EVENT, "Loaded DATA from JSON");
+				}
+				else{
+					Log(TAG_ERROR, "JSON parsing fail! Creating new one...");
+					file.close();
+					resetConfigFile();
+				}
+			}
+			else{
+				Log(TAG_ERROR, "Filed opening file, creating new one...");
+				file.close();
+				resetConfigFile();
+			}
+			file.close();
+		}
+		else{
+			Log(TAG_ERROR, "config file doesn't exist. Creating new one...");
+			resetConfigFile();
+		}
+	}
+	else{
+		Log(TAG_ERROR, "SPIFFS begin fail");
+	}
+	SPIFFS.end();
+}
+
+float getVcc(){
+	float vccVolt = ((float)ESP.getVcc()) / 1024;
+	if(vccVolt > (float)4.2){
+		vccVolt = (float)4.2;
+	}
+	if(vccVolt < (float)3){
+		vccVolt = 3;
+	}
+	Log(TAG_DATA, "Vcc: " + String((float)vccVolt));
+	return (float)vccVolt;
+}
+
+void saveToSendState(bool main, bool left, bool right){
+	StaticJsonBuffer<300> buff;
+	JsonObject& json = buff.createObject();
+	json["main"] = main;
+	json["left"] = left; // TODO
+	json["right"] = right; // TODO
+	json["voltage"] = getVcc();
+	json["button-software-version-device"] = softwareVersion;
+	json.printTo(messageToSend);
+	weNeedToSend = 1;
+	Log(TAG_EVENT, "Saved data to send and checked weNeedToSend flag to true");
+}
+
+void setup() {
+	pinMode(PIN_STANDBY, OUTPUT);
+	digitalWrite(PIN_STANDBY, 1);
+
+	pinMode(PIN_MAIN, INPUT_PULLUP);
+	if(digitalRead(PIN_MAIN)){
+		saveToSendState(true, false, false); // TODO
+	}
+
+	Serial.begin(115200);
+	pinMode(PIN_LED_GREEN, OUTPUT);
+	pinMode(PIN_LED_RED, OUTPUT);
+	setLed(1, 1);
+	pinMode(2, OUTPUT);
+    digitalWrite(2, 1);
+	Log(TAG_DATA, "Last reset: " + ESP.getResetReason());
+	loadDataFromFS();
+	WiFi.disconnect();
+
+	Log(TAG_DATA, "Freq: " + String(ESP.getCpuFreqMHz()));
+
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+            type = "sketch";
+        else // U_SPIFFS
+            type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("ERROR[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+}
+
+bool asyncButtonCheck(byte pin, bool wantedState = 0, unsigned long ignoreTime = 1000){
+    if(digitalRead(pin) == wantedState &&
+        ((millis() - timersButton[pin]) > ignoreTime)){
+        timersButton[pin] = millis();
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void asyncWifiConnectHandle(){
+    if(!tryingToConnect && !WiFi.isConnected()){
+		Log(TAG_DATA, "WiFi begin, SSID: " + wifiName + ", PASS: " + wifiPass);
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(wifiName.c_str(), wifiPass.c_str());
+        tryingToConnect = 1;
+    }
+    if(WiFi.isConnected() && tryingToConnect){
+		Log(TAG_EVENT, "Connected to wifi!");
+		Log(TAG_DATA, "IP: " + WiFi.localIP().toString());
+		ArduinoOTA.setHostname("pizza-sms-button");
+        ArduinoOTA.begin();
+
+        tryingToConnect = 0;
+    }
+}
+
+void asyncDataSending(){
+	if(weNeedToSend && WiFi.isConnected()){
+		//doingSomething();
+		if((millis() - sendingLastTry) > 5000){
+			Log(TAG_EVENT, "Trying to find mDNS...");
+			int a = MDNS.queryService("pizza-app", "tcp");
+			Log(TAG_DATA, String(a) + " host(s) found");
+			for(int x = 0; x < a; x++){
+				Log(TAG_DATA, "Host number " + String(x) + ": \n" +
+							MDNS.IP(x).toString() + " \n" +
+							String(MDNS.port(x)) + " \n" +
+							MDNS.hostname(x));
+				HTTPClient http;
+				http.begin(MDNS.IP(x).toString(), MDNS.port(x));
+				int code = http.POST(messageToSend);
+				Log(TAG_DATA, "Http code: " + String(code));
+				String received = http.getString();
+				Log(TAG_DATA, received);
+				if(code >= 200 && code < 300){
+					messageToSend = "";
+					weNeedToSend = 0;
+					sendingCount = 0;
+					Log(TAG_EVENT, "Message send succes!");
+					// you can't order pizza for next 15 seconds, for safety ;)
+					setLed(1, 0);
+					delay(15000);
+					goToSleep();
+					return;
+				}
+			}
+			sendingLastTry = millis();
+		}
+	}
+}
+
+void loop() {
+	if(wifiName.length() <= 0 && wifiPass.length() <= 0){
+
+	}
+	else{
+		asyncWifiConnectHandle();
+		asyncDataSending();
+		if(asyncButtonCheck(PIN_MAIN, 1)){
+			saveToSendState(true, false, false); // TODO
+		}
+
+		if((millis() - timerGoToSleep) > 30000){
+			goToSleep();
+		}
+	}
+	ArduinoOTA.handle();
+}
